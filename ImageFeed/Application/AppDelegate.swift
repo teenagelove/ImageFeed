d34ad845780cd0aae1da915a51f5e1a2a4ff19ae
teenagelove <dani4upa586@gmail.com>
2025-02-05 20:23:20 +0900

    Fixed warnings & deleted the main storyboard key

--- ImageFeed/Application/AppDelegate.swift
+++ ImageFeed/Application/AppDelegate.swift
@@ -22,7 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
-        let sceneConfiguration = UISceneConfiguration(name: "Main", sessionRole: connectingSceneSession.role)
+        let sceneConfiguration = UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
         sceneConfiguration.delegateClass = SceneDelegate.self
         return sceneConfiguration
     }
